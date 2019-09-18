50705913945b7d227deca59ac0b77aa4ce66db3c
Kenta <kkpineapple12@gmail.com>
2019-09-18 16:39:15 +0900

    added asgi

--- mysite/routing.py
+++ mysite/routing.py
@@ -1,11 +1,6 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
-import django
-import os
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-django.setup()
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
