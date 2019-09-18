c24546ed3762f0c9352447af7b63b3ff9a48af7a
Kenta <kkpineapple12@gmail.com>
2019-09-18 16:18:45 +0900

    asgi

--- mysite/routing.py
+++ mysite/routing.py
@@ -2,6 +2,8 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
     'websocket': AuthMiddlewareStack(
