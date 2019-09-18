73fc1a235514cf8064cacd133d6b29cdb7e71e1d
Kenta <kkpineapple12@gmail.com>
2019-09-18 16:24:40 +0900

    little change

--- mysite/routing.py
+++ mysite/routing.py
@@ -1,8 +1,10 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+django.setup()
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
