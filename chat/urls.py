ad3cad5829e6c0959227ad122f5516b4e5fad13b
Kenta <kkpineapple12@gmail.com>
2019-09-21 11:29:39 +0900

    add footer, urls, and views but not applied yet

--- chat/urls.py
+++ chat/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<str:room_name>/', views.room, name='room'),
+    path('roomlist', views.roomlist, name='room_list'),
+    path('login', views.login, name='login'),
+    path('user_create', views.usercreate, name='user_create'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -9,3 +9,12 @@ def room(request, room_name):
     return render(request, 'chat/room.html', {
         'room_name_json': mark_safe(json.dumps(room_name))
     })
+
+def roomlist(request):
+    return render(request, 'chat/room_list.html', {})
+
+def login(request):
+    return render(request, 'chat/login.html', {})
+
+def usercreate(request):
+    return render(request, 'chat/user_create', {})
